2b86448e2aeba3759ea4b7428b99a076610c12c2
koheisaito39 <kohei100315@gmail.com>
2016-04-24 12:17:58 +0000

    revert f644d6880bb9fea20d6b13b2703b32c7fea9b582

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :notes
   root :to => 'notes#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your rountes lay out with "rake routes".
 
