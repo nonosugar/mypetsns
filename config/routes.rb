582adf42b5903e184ac2fae772a6b74186940222
koheisaito39 <kohei100315@gmail.com>
2016-04-24 11:43:29 +0000

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :notes
   root :to => 'notes#new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your rountes lay out with "rake routes".
 
