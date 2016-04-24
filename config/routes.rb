5590b2b6f364f6491b6b85ff3cc2b9efc2f4883c
koheisaito39 <kohei100315@gmail.com>
2016-04-24 11:35:14 +0000

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :notes
+  root 'notes#new'
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your rountes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
