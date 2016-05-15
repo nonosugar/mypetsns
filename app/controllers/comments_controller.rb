d4f3f95076645a6bf7a099116cd50b3867968084
koheisaito39 <kohei100315@gmail.com>
2016-05-15 11:57:45 +0000

    コメント削除の機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,19 @@ class CommentsController < ApplicationController
 	# 	render 'notes/show'
 	# end
 
-		def commentscreate
+	def commentscreate
 	  @note = Note.find(params[:note_id])
 	  @comment = current_user.comments.build(note_id:@note.id)
 	  @comment.content = params[:content]
 	  @comment.save
 	  render 'notes/show'
 	end
+
+	def commentsdestroy
+		@comment = Comment.find(params[:comment_id])
+		@note = @comment.note
+		@comment.destroy
+		render 'notes/show'
+	end
+
 end
