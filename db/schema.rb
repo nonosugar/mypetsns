427ac86a070dfebc25a1a5cfb364aa29025356bb
koheisaito39 <kohei100315@gmail.com>
2016-06-26 10:03:41 +0000

    fix coflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160626033045) do
-
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -65,9 +63,7 @@ ActiveRecord::Schema.define(version: 20160626033045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "best_a"
-
     t.string   "image"
-
   end
 
   create_table "relationships", force: :cascade do |t|
